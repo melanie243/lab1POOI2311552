d9b4304d6f18c76f1dacbb5dcd6d258513ec23cb
melanie243 <142445444+melanie243@users.noreply.github.com>
2023-09-02 18:30:11 -0500

    Add files via upload

---
+++ ejercicio01/src/ejercicio01/ejercicio01.java
@@ -0,0 +1,12 @@
+
+package ejercicio01;
+
+public class ejercicio01 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        if (6 < 6 * 5)
+	    System.out.print("Hello");
+	    System.out.print(" There");
+	    //LA RESPUESTA ES ; Hello There
+    }
+}
