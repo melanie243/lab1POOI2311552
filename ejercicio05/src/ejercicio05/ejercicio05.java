cbd99cfd379d92c1a4787f137dee46e000743808
melanie243 <142445444+melanie243@users.noreply.github.com>
2023-09-02 18:54:08 -0500

    Add files via upload

---
+++ ejercicio05/src/ejercicio05/ejercicio05.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio05;
+
+/**
+ *
+ * @author melanieserpa
+ */
+public class ejercicio05 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=1, y=2, z=3;
+		
+		if(x>y){
+			System.out.println("####");}
+                else{
+			System.out.println("&&&&");
+		        System.out.println("****");}
+		    //LA RESPUESTA ES ; &&&& ****
+		    }
+		    }
