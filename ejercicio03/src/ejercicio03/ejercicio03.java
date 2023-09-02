fc366ca88b5262c4722e85dac9c744ad52d2b75e
melanie243 <142445444+melanie243@users.noreply.github.com>
2023-09-02 18:31:29 -0500

    Add files via upload

---
+++ ejercicio03/src/ejercicio03/ejercicio03.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio03;
+
+/**
+ *
+ * @author melanieserpa
+ */
+public class ejercicio03 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+		int x=1, y=2, z=3;
+		
+		if (x < z)
+			System.out.println("*");
+		else if (x == z)
+			System.out.println("&");
+		else
+			System.out.println("$");
+		//LA RESPUESTA ES ; *
+    }
+}
