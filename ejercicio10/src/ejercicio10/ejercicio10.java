204cf5b4bb996461983a549ddf6599a727cce755
melanie243 <142445444+melanie243@users.noreply.github.com>
2023-09-02 19:25:03 -0500

    Add files via upload

---
+++ ejercicio10/src/ejercicio10/ejercicio10.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio10;
+
+/**
+ *
+ * @author melanieserpa
+ */
+public class ejercicio10 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=1, y=2, z=3;
+		
+		if (x < z)
+			System.out.println("*");
+		else if (x==z)
+			System.out.println("&");
+		else
+			System.out.println("$");
+		//LA RESPUESTA ES ; *
+		
+	}
+
+}
+
