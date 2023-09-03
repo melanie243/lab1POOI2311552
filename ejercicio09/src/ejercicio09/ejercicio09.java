d327b52a19d8f711ffdbf035579696f06050aaf8
melanie243 <142445444+melanie243@users.noreply.github.com>
2023-09-02 19:20:09 -0500

    Add files via upload

---
+++ ejercicio09/src/ejercicio09/ejercicio09.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio09;
+
+/**
+ *
+ * @author melanieserpa
+ */
+public class ejercicio09 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=1, y=2, z=3;
+		
+		if ('x' > 'y' || 66 > (int)('A'))
+			System.out.println("#*#");
+		    //LA RESPUESTA ES #*#
+		
+	}
+
+}
+
