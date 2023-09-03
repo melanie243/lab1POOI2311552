64ebd408dce65674c94fa8b5aa96b3655bb3c96f
melanie243 <142445444+melanie243@users.noreply.github.com>
2023-09-02 19:13:58 -0500

    Add files via upload

---
+++ ejercicio08/src/ejercicio08/ejercicio08.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio08;
+
+/**
+ *
+ * @author melanieserpa
+ */
+public class ejercicio08 {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=1, y=2, z=3;
+		
+		if (x<y){
+			System.out.println("####");
+			System.out.println("****");
+			
+		}
+		else
+			System.out.println("&&&&");
+		//LA RESPUESTA ES ; #### ****
+	}
+
+}
+
